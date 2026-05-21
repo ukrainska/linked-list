04a96ff39e4f3c365366c193fef47c5514f6d9e6
ukrainska <215995465+ukrainska@users.noreply.github.com>
2026-05-21 22:39:05 +0900

    Add two classes to a file

---
+++ example.js
@@ -0,0 +1,13 @@
+class LinkedList {
+    append(value) {
+
+    }
+}
+
+class Node {
+    constructor(value, nextNode)
+    {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
