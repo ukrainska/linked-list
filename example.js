d4e39126cc706e18014bb6228aa092bb07d6c44e
ukrainska <215995465+ukrainska@users.noreply.github.com>
2026-05-21 22:50:27 +0900

    Add a function append to the linked list

--- example.js
+++ example.js
@@ -1,13 +1,29 @@
 class LinkedList {
-    append(value) {
+    constructor(){
+        this.head = null;
+    }
 
+    append(value) {
+        const newNode = new Node(value);
+
+        if(this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+
+        while (current.next !== null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
     }
 }
 
 class Node {
     constructor(value, nextNode)
     {
-        this.value = value;
-        this.nextNode = nextNode;
+        this.value = null;
+        this.nextNode = null;
     }
 }
